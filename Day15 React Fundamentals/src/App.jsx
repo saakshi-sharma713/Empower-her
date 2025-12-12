c561a31e3f1b8f782edf4b278f81c194f2763cc0
Saakshi Sharma <saakshi7466@gmail.com>
2025-12-12 14:26:18 +0530

    Commit

---
+++ Day15 React Fundamentals/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import FirstComponent from './FirstComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <FirstComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ Day15 React Fundamentals/src/FirstComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FirstComponent = () => {
+  return (
+    <h2>This is my first React component</h2>
+  )
+}
+
+export default FirstComponent
