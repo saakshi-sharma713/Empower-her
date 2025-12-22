4aa9b6f7893dc5ad5fc9882ec747b711749744f1
Saakshi Sharma <saakshi7466@gmail.com>
2025-12-22 23:19:10 +0530

    commit

---
+++ Day19/Ques2/src/App.jsx
@@ -0,0 +1,12 @@
+import Component1 from "./Component1";
+
+function App() {
+  return (
+    <div>
+      <h2>Understanding Props Drilling</h2>
+      <Component1 />
+    </div>
+  );
+}
+
+export default App;
